52ac6a52257d3cbbb087d7869ca8f9dffee58cce
bsegers <briansegs@gmail.com>
2025-10-01 11:02:42 -0400

    fix: Icon rendering logic

--- src/features/shared/components/Header/ThemeToggle.tsx
+++ src/features/shared/components/Header/ThemeToggle.tsx
@@ -13,11 +13,8 @@ export function ThemeToggle() {
       variant="ghost"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
-      {theme === 'light' ? (
-        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      ) : (
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-      )}
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:-rotate-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:rotate-100" />
     </Button>
   )
 }
