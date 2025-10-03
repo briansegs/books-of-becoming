4c549ed07300159736d4aab7d58b31c2e29ae377
bsegers <briansegs@gmail.com>
2025-10-03 17:09:45 -0400

    Link to user paths

--- src/features/shared/components/Header/HeaderUserItems.tsx
+++ src/features/shared/components/Header/HeaderUserItems.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { Authenticated, Unauthenticated, AuthLoading } from 'convex/react'
-import { SignInButton, UserButton } from '@clerk/nextjs'
+import { SignInButton, UserButton, useUser } from '@clerk/nextjs'
 import { Button } from '@/features/shared/components/ui/button'
 import { Book, HomeIcon, User } from 'lucide-react'
 import Link from 'next/link'
 
 export function HeaderUserItems() {
+  const { user } = useUser()
+
   return (
     <>
       <Unauthenticated>
@@ -35,13 +37,13 @@ export function HeaderUserItems() {
 
       <Authenticated>
         <div className="flex items-center gap-2">
-          <Link href="/journals">
+          <Link href={`/journals/${user?.id}`}>
             <Button variant="ghost">
               <Book /> <span className="hidden sm:block">Journals</span>
             </Button>
           </Link>
 
-          <Link href="/dashboard">
+          <Link href={`/dashboard/${user?.id}`}>
             <Button variant="ghost">
               <HomeIcon /> <span className="hidden sm:block">Dashboard</span>
             </Button>
