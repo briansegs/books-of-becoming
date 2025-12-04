478647d3b3b4b6eb65e1c53e901456e9160ccf0e
bsegers <briansegs@gmail.com>
2025-12-04 16:52:01 -0500

    fix: Remove copied code

--- convex/entry.ts
+++ convex/entry.ts
@@ -1,12 +1,6 @@
 import { v } from 'convex/values'
-import { mutation, query } from './_generated/server'
+import { mutation } from './_generated/server'
 import { getAuthenticatedUser, getCurrentUserJournal } from './_utils'
-import {
-  journalType,
-  journalColors,
-  journalTextColors,
-  journalBackgrounds,
-} from './lib/journalSchema'
 
 export const create = mutation({
   args: {
@@ -31,52 +25,5 @@ export const create = mutation({
         entriesCount: (journal.entriesCount ?? 0) + 1,
       })
     }
-  },
-})
-
-export const remove = mutation({
-  args: { id: v.id('journals') },
-  handler: async (ctx, { id }) => {
-    const currentUser = await getAuthenticatedUser(ctx)
-
-    const journal = await getCurrentUserJournal({ ctx, currentUser, id })
-
-    await ctx.db.delete(journal._id)
-  },
-})
-
-export const update = mutation({
-  args: {
-    id: v.id('journals'),
-    data: v.object({
-      title: v.string(),
-      type: journalType,
-      color: journalColors,
-      textColor: journalTextColors,
-      background: journalBackgrounds,
-    }),
-  },
-  handler: async (ctx, { id, data }) => {
-    const currentUser = await getAuthenticatedUser(ctx)
-
-    const journal = await getCurrentUserJournal({ ctx, currentUser, id })
-
-    await ctx.db.patch(journal._id, {
-      updatedAt: Date.now(),
-      ...data,
-    })
-  },
-})
-
-export const get = query({
-  args: {
-    id: v.id('journals'),
-  },
-  handler: async (ctx, { id }) => {
-    await getAuthenticatedUser(ctx)
-
-    const journal = await ctx.db.get(id)
-
-    return journal
   },
 })
