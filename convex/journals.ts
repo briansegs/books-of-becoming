ed25e470ca134488e2067bceea63071294a10c02
bsegers <briansegs@gmail.com>
2025-12-08 18:56:01 -0500

    refactor: Optimize query

--- convex/journals.ts
+++ convex/journals.ts
@@ -23,20 +23,23 @@ export const getJournalsWithEntriesCount = query({
       .withIndex('by_userId', (q) => q.eq('userId', currentUser._id))
       .collect()
 
-    const journalsWithEntriesCount = await Promise.all(
-      journals.map(async (journal) => {
-        const entries = await ctx.db
-          .query('entries')
-          .withIndex('by_journalId', (q) => q.eq('journalId', journal._id))
-          .collect()
+    const allEntries = await ctx.db
+      .query('entries')
+      .withIndex('by_userId', (q) => q.eq('userId', currentUser._id))
+      .collect()
 
-        return {
-          ...journal,
-          entriesCount: entries.length,
-        }
-      }),
+    // Group entries by journalId
+    const entryCounts = allEntries.reduce(
+      (acc, entry) => {
+        acc[entry.journalId] = (acc[entry.journalId] || 0) + 1
+        return acc
+      },
+      {} as Record<string, number>,
     )
 
-    return journalsWithEntriesCount
+    return journals.map((journal) => ({
+      ...journal,
+      entriesCount: entryCounts[journal._id] || 0,
+    }))
   },
 })
