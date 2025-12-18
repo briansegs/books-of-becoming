230c4b07b0acc1d667bd81c28a640192339353a5
bsegers <briansegs@gmail.com>
2025-12-18 10:36:47 -0500

    fix: Incorrect time in porduction for entries

--- src/app/journal/[id]/page.tsx
+++ src/app/journal/[id]/page.tsx
@@ -57,7 +57,6 @@ export default async function JournalPage({ params }: Args) {
 
     entriesCount = ungroupedEntries.length
 
-    const todayKey = format(new Date(), 'yyyy-MM-dd')
     const dailyEntryGroups: DailyEntryGroup[] = []
 
     ungroupedEntries.forEach((entry) => {
@@ -72,10 +71,6 @@ export default async function JournalPage({ params }: Args) {
 
       entryGroup.entries.push(entry)
     })
-
-    if (!dailyEntryGroups.find((group) => group.date === todayKey)) {
-      dailyEntryGroups.push({ date: todayKey, entries: [] })
-    }
 
     dailyEntries = dailyEntryGroups.sort(
       (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
